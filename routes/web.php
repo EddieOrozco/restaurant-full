e7d8f5989139359e8edeac609763fb06ab6fdc9f
EddieOrozco <orozcoe230@gmail.com>
2020-04-09 13:55:17 -0500

    Creating views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/app', function () {
+    return view('test');
+});
+Route::get('/menu', function () {
+    return view('menu/index');
+});
